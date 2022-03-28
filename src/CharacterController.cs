832f5442b8be14cc95fa6cfd9ae2f8136dc2b171
Daniel Schulz <faulolio@gmail.com>
2022-03-28 19:55:21 +0200

    Add basic movement

--- src/CharacterController.cs
+++ src/CharacterController.cs
@@ -3,6 +3,7 @@ using Godot;
 namespace Freeblob {
     public class CharacterController : KinematicBody {
         Vector3 velocity;
+        Vector2 movement;
         Vector2 look;
 
         [Export]
@@ -15,12 +16,27 @@ namespace Freeblob {
             Input.SetMouseMode(Input.MouseMode.Captured);
             body = this;
             head = GetNode<Spatial>("Head");
+
+            look.x = body.RotationDegrees.y;
+            look.y = head.RotationDegrees.x;
         }
 
         public override void _Process(float delta) {
+            movement = GetMovement();
+
             body.RotationDegrees = new Vector3(0, look.x, 0);
             head.RotationDegrees = new Vector3(look.y, 0, 0);
+
+            velocity.x = movement.x;
+            velocity.z = movement.y;
+
+            MoveAndSlide(velocity);
         }
+
+        Vector2 GetMovement() => new Vector2 {
+            x = Input.GetActionStrength("avatar_walk_right") - Input.GetActionStrength("avatar_walk_left"),
+            y = Input.GetActionStrength("avatar_walk_forward") - Input.GetActionStrength("avatar_walk_back")
+        };
 
         public override void _Input(InputEvent eve) {
             base._Input(eve);
