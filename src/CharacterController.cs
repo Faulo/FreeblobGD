dbe0323ebf4aba684a7059e218b57b7cca608dd3
Daniel Schulz <faulolio@gmail.com>
2022-03-28 19:18:23 +0200

    Add mouse input

--- src/CharacterController.cs
+++ src/CharacterController.cs
@@ -5,25 +5,29 @@ namespace Freeblob {
         Vector3 velocity;
         Vector2 look;
 
-        Transform body;
-        Transform head;
+        [Export]
+        Vector2 inputGain = Vector2.One;
+
+        Spatial body;
+        Spatial head;
 
         public override void _Ready() {
             Input.SetMouseMode(Input.MouseMode.Captured);
-            body = Transform;
-            head = GetNode<Spatial>("Head").Transform;
+            body = this;
+            head = GetNode<Spatial>("Head");
         }
 
         public override void _Process(float delta) {
-            body.basis = new Basis(new Vector3(0, look.x, 0));
-            head.basis = new Basis(new Vector3(look.y, 0, 0));
+            body.RotationDegrees = new Vector3(0, look.x, 0);
+            head.RotationDegrees = new Vector3(look.y, 0, 0);
         }
 
         public override void _Input(InputEvent eve) {
             base._Input(eve);
 
             if (eve is InputEventMouseMotion motion && Input.GetMouseMode() == Input.MouseMode.Captured) {
-                look += motion.Relative;
+                look -= motion.Relative * inputGain;
+                look.y = Mathf.Clamp(look.y, -90, 90);
             }
         }
     }
